da894272a16ebd8baac3b785ffed900d9429f327
Oleksandr Filippov <filiczini@gmail.com>
2025-10-23 15:26:55 +0300

    New logic with tasks moves

--- src/components/pages/BoardPage.jsx
+++ src/components/pages/BoardPage.jsx
@@ -1,20 +1,23 @@
-const statuses = ["BACKLOG", "TODO", "IN_PROGRESS", "DONE"];
-const statusNames = {
-  BACKLOG: "Backlog",
-  TODO: "To-do",
-  IN_PROGRESS: "In-progress",
-  DONE: "Done",
-};
+import { useEffect, useState } from "react";
+import { api } from "../../api/axios";
 
-const mockTasks = [
-  { id: 1, title: "To configure project for future work", status: "BACKLOG" },
-  { id: 2, title: "To learn JavaScript for skill matrix", status: "BACKLOG" },
-  { id: 3, title: "To learn React for skill matrix", status: "BACKLOG" },
-];
+const statuses = ["BACKLOG", "TODO", "IN_PROGRESS", "DONE"];
+const statusNames = { BACKLOG: "Backlog", TODO: "To-do", IN_PROGRESS: "In-progress", DONE: "Done" };
 
 export default function BoardPage() {
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    api.get("/tasks").then((res) => setTasks(res.data));
+  }, []);
+
+  const handleAdvance = async (id) => {
+    const res = await api.post(`/tasks/${id}/advance`);
+    setTasks((prevArr) => prevArr.map((task) => (task.id === id ? res.data : task)));
+  };
+
   const columns = { BACKLOG: [], TODO: [], IN_PROGRESS: [], DONE: [] };
-  for (const task of mockTasks) columns[task.status]?.push(task);
+  for (const task of tasks) columns[task.status]?.push(task);
+
   return (
     <div className="board-container">
       <h2>Дошка задач</h2>
@@ -25,6 +28,11 @@ export default function BoardPage() {
             {columns[status].map((task) => (
               <div key={task.id} className="task">
                 {task.title}
+                {task.status !== "DONE" && (
+                  <button onClick={() => handleAdvance(task.id)} style={{ float: "right" }}>
+                    →
+                  </button>
+                )}
               </div>
             ))}
           </div>
